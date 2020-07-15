049f5c891cb460a194d1b9b4cb8ed0fbfd5c67c6
joesanthosh24 <joe.santhosh88@yahoo.com>
2020-07-15 07:50:16 -0400

    Made home page jumbotron and card

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,48 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="home">
+    <b-jumbotron fluid header-tag="div">
+      <template v-slot:header>Kanji Samurai (漢字侍)</template>
+      <template v-slot:lead>App leveraging Kanji API to help learn</template>
+      <hr class="my-4" />
+      <div class="home__buttons">
+        <b-button variant="primary">Kanji By Grade</b-button>
+      </div>
+    </b-jumbotron>
+    <b-card-group deck>
+      <div class="grades">
+        <b-card
+          img-src="https://ih1.redbubble.net/image.86481084.7993/raf,750x1000,075,t,000000:44f0b734a5.jpg"
+          img-alt="Samurai Kanji"
+          img-left
+          img-height="100"
+          img-width="100"
+          class="mb-3"
+        >
+          <b-card-text>Some quick example text to build on the card and make up the bulk of the card's content.</b-card-text>
+        </b-card>
+      </div>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
-export default {};
 </script>
 
-<style>
+<style scoped>
+.jumbotron > .container > .display-3 {
+  text-align: center;
+}
+.jumbotron > .container > p {
+  text-align: center;
+}
+.home__buttons {
+  display: flex;
+  justify-content: center;
+}
+.grades {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-left: 25%;
+}
 </style>
