aa778126ac9b74e35b3a6567ca59716aa0eafc9e
joesanthosh24 <joe.santhosh88@yahoo.com>
2020-07-16 18:44:58 -0400

    Added axios module to send requests to kanji api. Set kanji/grades/_level page to hold different kanji based on grade level inside cards.

--- pages/kanjis/grades/_level/index.vue
+++ pages/kanjis/grades/_level/index.vue
@@ -1,10 +1,63 @@
 <template>
-  <h1>Grade {{$route.params.level}} Kanji</h1>
+  <div>
+    <div class="main-text">
+      <h1 class="main-text">Grade {{$route.params.level}} Kanji</h1>
+      <p>Amount: {{kanjis.length}} Kanji</p>
+    </div>
+    <!-- <ul>
+      <li v-for="kanji in kanjis" :key="kanji">{{kanji}}</li>
+    </ul>-->
+    <div class="kanjis">
+      <b-card
+        header-tag="header"
+        :header="(index + 1).toString()"
+        footer-tag="footer"
+        class="mb-2 mr-3 ml-3"
+        v-for="(kanji, index) in kanjis"
+        :key="index"
+      >
+        <template v-slot:footer>
+          <b-button variant="primary">Add To Study</b-button>
+        </template>
+        <b-card-title>
+          <h3 class="kanji-picture"><font-awesome-icon :icon="['fas', 'adjust']" />{{kanji}}</h3>
+        </b-card-title>
+      </b-card>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  asyncData({ $axios, params }) {
+    return $axios.$get(`/kanji/grade-${params.level}`).then(data => {
+      return {
+        kanjis: data
+      };
+    });
+  }
 };
 </script>
-<style>
+<style scoped>
+.main-text {
+  text-align: center;
+}
+.kanjis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: space-between;
+}
+.card-body {
+  text-align: center;
+  font-weight: bold;
+}
+.card-title:hover {
+  cursor: pointer;
+  opacity: 0.6;
+}
+.kanji-picture {
+  font-weight: bold;
+  font-size: 50px;
+}
 </style>
