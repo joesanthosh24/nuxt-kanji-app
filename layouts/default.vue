aa5c6acb3a55045ca5aad94ad9f06001cacf730b
joesanthosh24 <joe.santhosh88@yahoo.com>
2020-07-26 21:20:40 -0400

    Added transition of navbar color on scroll and made navbar stick to top at all times when scrolling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <b-navbar variant="primary" type="dark">
+  <div class="navbar-div">
+    <b-navbar class="navbar" :variant="variant" type="dark">
       <nuxt-link to="/">
         <b-navbar-brand>Home</b-navbar-brand>
       </nuxt-link>
@@ -13,6 +13,7 @@
         <b-nav-item>Sign Up</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
+    <div class="spacing"></div>
     <nuxt />
   </div>
 </template>
@@ -33,11 +34,40 @@ export default {
         { text: "Grade 4", link: "/kanjis/grades/4" },
         { text: "Grade 5", link: "/kanjis/grades/5" },
         { text: "Grade 6", link: "/kanjis/grades/6" }
-      ]
+      ],
+      variant: 'primary'
     };
+  },
+  methods: {
+    scroll(event) {
+      console.log(window.scrollY);
+      if(window.scrollY > 25) {
+        this.variant = 'secondary';
+      }
+      else {
+        this.variant = 'primary'
+      }
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.scroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.scroll);
   }
 };
 </script>
 
-<style>
+<style scoped>
+.navbar {
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  top: 0;
+  transition-timing-function: ease-in;
+  transition: all 0.65s;
+}
+.spacing {
+  height: 80px;
+}
 </style>
