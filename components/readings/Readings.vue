30a73beee8e7292e12f2bc837198d69c7e2a1424
joesanthosh24 <joe.santhosh88@yahoo.com>
2020-07-27 12:20:01 -0400

    Fixed dependency problem with core-js. Added conditional to say No readings if there aren't any readings for either Kun or On

--- components/readings/Readings.vue
+++ components/readings/Readings.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="d-block p-2 bg-default text-primary text-uppercase ml-3 mr-3 mb-4">
     {{readingType}} Readings:
-    <span
-      :class="index === 0 ? 'character__dataHeisig' : ''"
-      v-for="(reading, index) in readings"
-      :key="reading"
-    >
-      {{reading}}
-      <span class="mr-2" v-show="index < readings.length - 1 && readings.length > 1">,</span>
-    </span>
+    <div v-if="readings.length > 0">
+      <span
+        :class="index === 0 ? 'character__dataHeisig' : ''"
+        v-for="(reading, index) in readings"
+        :key="reading"
+      >
+        {{reading}}
+        <span class="mr-2" v-show="index < readings.length - 1 && readings.length > 1">,</span>
+      </span>
+    </div>
+    <div v-if="readings.length === 0">
+      <span class="no-reading">No {{readingType}} readings</span>
+    </div>
   </div>
 </template>
 
@@ -17,18 +22,21 @@ export default {
   props: {
     readingType: {
       type: String,
-      required: true
+      required: true,
     },
     readings: {
       type: Array,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 };
 </script>
 
 <style scoped>
 .character__dataHeisig {
   margin-left: 15px;
+}
+.no-reading {
+  color: rgb(216, 17, 17);
 }
 </style>
